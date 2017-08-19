90a0da4e90ccd4007ad6ba3162fdca1d8f36632e
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-19 14:29:21 +0800

    Added Player class

---
+++ main/Webcrawler/Webcrawler/CustomObjects/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webcrawler.CustomObjects
+{
+    class Player
+    {
+
+        public string name { get; set; }
+
+        public string link { get; set; }
+
+        public float points { get; set; }
+
+    }
+}
