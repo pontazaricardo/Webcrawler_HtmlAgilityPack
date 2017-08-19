289cd00264f084314c53f64f52401b51c1451b66
pontazaricardo <pontaza.ricardo@gmail.com>
2017-08-19 15:33:41 +0800

    Creation of objects based on information

--- main/Webcrawler/Webcrawler/Program.cs
+++ main/Webcrawler/Webcrawler/Program.cs
@@ -21,6 +21,13 @@ namespace Webcrawler
         {
             string mainSite = "http://www.espn.com/nba/statistics";
 
+            List<Player> listOfPlayers_offensiveLeaders = new List<Player>();
+            List<Player> listOfPlayers_defensiveLeaders = new List<Player>();
+            List<Player> listOfPlayers_assists = new List<Player>();
+            List<Player> listOfPlayers_blocks = new List<Player>();
+            List<Player> listOfPlayers_fieldGoal = new List<Player>();
+            List<Player> listOfPlayers_steals = new List<Player>();
+
             //We first crawl the site
             HtmlWeb site = new HtmlWeb();
             HtmlDocument htmlDocument = site.Load(@mainSite);
@@ -32,6 +39,42 @@ namespace Webcrawler
             //We get the following:
             //1. Offensive leaders
             HtmlNode node_offensiveLeaders = leaderBoards_01[0];
+            HtmlNode node_offensiveLeaders_div = node_offensiveLeaders.SelectNodes("div[@class='mod-content']").FirstOrDefault();
+            HtmlNode node_offensiveLeaders_table = node_offensiveLeaders_div.SelectNodes("table").FirstOrDefault();
+
+            for (int i = 1; i < node_offensiveLeaders_table.SelectNodes("tr").Count(); i++) //We skip the first row
+            {
+                HtmlNode row = node_offensiveLeaders_table.SelectNodes("tr")[i];
+
+                HtmlNode cell_name = null;
+                HtmlNode cell_points = null;
+
+                if (i == 1)
+                {
+                    cell_name = row.SelectNodes("td|th")[1];
+                    cell_points = row.SelectNodes("td|th")[2];
+                }
+                else
+                {
+                    cell_name = row.SelectNodes("td|th")[0];
+                    cell_points = row.SelectNodes("td|th")[1];
+                }
+
+                HtmlNode cell_name_aTag = cell_name.SelectNodes("a").FirstOrDefault();
+
+                string link = cell_name_aTag.Attributes["href"].Value.ToString();
+                string name = cell_name_aTag.InnerText;
+                float points = float.Parse(cell_points.InnerText);
+
+                Player player = new Player();
+                player.name = name;
+                player.link = link;
+                player.points = points;
+
+                listOfPlayers_offensiveLeaders.Add(player);
+
+            }
+
 
 
         }
